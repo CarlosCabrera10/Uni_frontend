dda953917ba92a61ee3e8620212a9ce7e6fa7e51
Carlos Cabrera <carlos.cabrera1@catolica.edu.sv>
2025-11-18 15:09:11 -0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div
+      style={{
+        padding: 20,
+        maxWidth: 400,
+        margin: "auto",
+        fontFamily: "sans-serif",
+        textAlign: "center"
+      }}
+    >
+      <h2>ADMIN Móvil</h2>
+      <p>Prueba para WebView Swift</p>
+    </div>
+  );
+}
+
+export default App;
